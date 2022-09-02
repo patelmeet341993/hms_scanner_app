7e33d0ce19c4c77c11f27039c4900b9ffde33b0b
Dishant <agrawaldishant29@gmail.com>
2022-09-02 16:16:03 +0530

    added constants related to Shared Preference, Gendeer, Admin User type, Firebase Nodes, Medicine Dose Time, Payment Modes and Payment Status

--- lib/configs/constants.dart
+++ lib/configs/constants.dart
@@ -2,4 +2,45 @@
 const String app_version = "1.0.0";
 
 //Shared Preference Keys
-const String appThemeMode = "themeMode";
+class SharePrefrenceKeys {
+  static const String appThemeMode = "themeMode";
+}
+
+class PatientGender {
+  static const String male = "Male";
+  static const String female = "Female";
+  static const String other = "Other";
+}
+
+class AdminUserType {
+  static const String admin = "Admin";
+  static const String doctor = "Doctor";
+  static const String pharmacy = "Pharmacy";
+  static const String laboratory = "Laboratory";
+  static const String reception = "Reception";
+}
+
+class FirebaseNodes {
+  static const String adminUsersCollection = "admin_users";
+  static const String patientCollection = "patient";
+  static const String visitsCollection = "visits";
+}
+
+class PrescriptionMedicineDoseTime {
+  static const String morning = "Morning";
+  static const String afternoon = "Afternoon";
+  static const String evening = "Evening";
+  static const String night = "Night";
+}
+
+class PaymentModes {
+  static const String cash = "Cash";
+  static const String upi = "UPI";
+  static const String creditCard = "Credit Card";
+  static const String debitCard = "Debit Card";
+}
+
+class PaymentStatus {
+  static const String pending = "Pending";
+  static const String paid = "Paid";
+}
