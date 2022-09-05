2be035711c7b6cfb9235f85b40f6ba308a20ef79
Dishant <agrawaldishant29@gmail.com>
2022-09-05 16:47:46 +0530

    added loggedInUser key in SharePrefrenceKeys to store loggedin user data

--- lib/configs/constants.dart
+++ lib/configs/constants.dart
@@ -4,6 +4,11 @@ const String app_version = "1.0.0";
 //Shared Preference Keys
 class SharePrefrenceKeys {
   static const String appThemeMode = "themeMode";
+  static const String loggedInUser = "loggedInUser";
+}
+
+class AppConstants {
+  static const List<String> userTypesForLogin = [AdminUserType.admin, AdminUserType.reception, AdminUserType.doctor, AdminUserType.pharmacy, AdminUserType.laboratory];
 }
 
 class PatientGender {
