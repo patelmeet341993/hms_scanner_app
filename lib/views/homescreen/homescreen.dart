07f32df20d7ca8b0fcf88ce6f64f0781b74685a3
Dishant <agrawaldishant29@gmail.com>
2022-09-05 16:49:09 +0530

    added logout button and showing logged in user name and role in HomeScreen

--- lib/views/homescreen/homescreen.dart
+++ lib/views/homescreen/homescreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../../controllers/firestore_controller.dart';
-import '../../utils/logger_service.dart';
+import '../../controllers/authentication_controller.dart';
+import '../../models/admin_user_model.dart';
+import '../../providers/admin_user_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "/HomeScreen";
@@ -18,20 +20,43 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text("Home Screen"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                AuthenticationController().logout(context: context);
+              },
+              icon: Icon(Icons.logout),
+            )
+          ],
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("Login Body"),
+              Text("Home Body"),
+              SizedBox(height: 20,),
+              Consumer<AdminUserProvider>(
+                builder: (BuildContext context, AdminUserProvider adminUserProvider, Widget? child) {
+                  AdminUserModel? adminUserModel = adminUserProvider.getAdminUserModel();
+                  if(adminUserModel == null) {
+                    return Text("Not Logged in");
+                  }
+                  return Column(
+                    children: [
+                      Text("User Name:${adminUserProvider.getAdminUserModel()!.name}"),
+                      Text("User Role:${adminUserProvider.getAdminUserModel()!.role}"),
+                    ],
+                  );
+                },
+              ),
+              SizedBox(height: 20,),
               FlatButton(
                 onPressed: () {
-                  FirestoreController().firestore.collection("temp").add({"dfgh" : "drdfgh"}).then((value) {
-                    Log().i("Document Created:${value.id}");
-                  });
+                  // VisitController().createDummyVisitDataInFirestore();
+                  // PatientController().createDummyPatientDataInFirestore();
                 },
-                child: Text("Add"),
+                child: Text("Create Visit"),
               ),
             ],
           ),
