8e481e17fbefc2f42cee30b1478ed57a00c21080
Dishant <agrawaldishant29@gmail.com>
2022-09-07 17:44:28 +0530

    added option to customize the LoadingWidget with different parameters

--- lib/views/common/components/loading_widget.dart
+++ lib/views/common/components/loading_widget.dart
@@ -2,20 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LoadingWidget extends StatelessWidget {
-  const LoadingWidget({Key? key}) : super(key: key);
+  final double boxSize, loaderSize;
+  final Color? backgroundColor, loaderColor;
+  final double borderRadius;
+  const LoadingWidget({
+    Key? key,
+    this.boxSize = 90,
+    this.loaderSize = 50,
+    this.backgroundColor,
+    this.loaderColor,
+    this.borderRadius = 20,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
 
     return Container(
-      height: 90,
-      width: 90,
+      height: boxSize,
+      width: boxSize,
       decoration: BoxDecoration(
-        color: themeData.colorScheme.onPrimary,
-        borderRadius: BorderRadius.circular(20)
+        color: backgroundColor ?? themeData.colorScheme.onPrimary,
+        borderRadius: BorderRadius.circular(borderRadius)
       ),
-      child: Center(child: LoadingAnimationWidget.fourRotatingDots(color: themeData.primaryColor, size: 50)),
+      child: Center(child: LoadingAnimationWidget.fourRotatingDots(color: loaderColor ?? themeData.primaryColor, size: loaderSize)),
     );
   }
 }
